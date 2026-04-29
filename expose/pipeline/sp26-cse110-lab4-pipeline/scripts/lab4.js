6f66d5b760c95d855a3247b77c317ff5124524e5
Hjkim118 <hok013@ucsd.edu>
2026-04-28 20:36:58 -0700

    Remove embedded pipeline repo

---
+++ expose/pipeline/sp26-cse110-lab4-pipeline/scripts/lab4.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * @param {*} num1, first number to add.
+ * @param {*} num2, second number to add.
+ * @param {*} add, boolean value to tell the function what to do.
+ * @returns The sum of the two numbers if add is true and false otherwise.
+ */
+function sumValues(num1, num2, add) {
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    typeof add !== "boolean"
+  ) {
+    return false;
+  }
+  if (add) {
+    return num1 + num2;
+  }
+  return false;
+}
+
+/**
+ *
+ * @param {*} prices, an array of the original price.
+ * @param {*} discount, a number between 0-1 to represent the discount.
+ * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
+ */
+function discountPrices(prices, discount) {
+  if (
+    !Array.isArray(prices) ||
+    prices.length === 0 ||
+    typeof discount !== "number"
+  ) {
+    return false;
+  }
+  let discounted = [];
+  for (let i = 0; i < prices.length; i++) {
+    discounted.push(prices[i] * (1 - discount));
+  }
+  return discounted;
+}
+
+module.exports = { sumValues, discountPrices };
